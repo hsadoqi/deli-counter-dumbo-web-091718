1cf3990a07f4060f54d1528930fd47b745846871
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 02:58:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,18 @@
 
 katz_deli = []
 
-def line(katz_deli)
+def line(katz_deli, names)
+  line = []
   if katz_deli.size == 0 
     puts "The line is currently empty."
+  elsif katz_deli.size > 0
+    names.each_with_index {|name, index| line << "#{index+1}. #{name}"}
+    puts "The line is currently: " + line.to_s
   end
 end
 
-def take_a_number(katz_deli, names)
-  line = []
-  if katz_deli.size > 0
-    names.each_with_index {|name, index| line << "#{index+1}. #{name}"}
-  end
-  puts "The line is currently: " + line.to_s
+def take_a_number
+  
 end 
 
 def now_serving
