5301abd41ffc3b571debdc1d9ea46c2fb0d40bfa
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 03:16:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(katz_deli, name)
   if katz_deli.size == 0
     katz_deli.push(name)
-    puts "Welcome, #{katz_deli[-1]}. You are currently #{katz_deli.size + 1} in line."
+    puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.size} in line."
 end
 end 
 
